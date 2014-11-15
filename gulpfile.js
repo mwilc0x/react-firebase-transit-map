b99f0ac54623daa417013208a4d2a195f945b51a
Mike Wilcox <mwilcox56@gmail.com>
2014-11-15 16:51:31 -0500

    bundle up css

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     del = require('del'),
     notify = require('gulp-notify'),
     browserSync = require('browser-sync'),
+    concatCss = require('gulp-concat-css'),
     reload = browserSync.reload,
     p = {
       jsx: './lib/scripts/app.jsx',
@@ -69,6 +70,7 @@ gulp.task('styles', function() {
     .pipe(changed(p.distCss))
     .pipe(autoprefixer('last 1 version'))
     .pipe(csso())
+    .pipe(concatCss('bundle.css'))
     .pipe(gulp.dest(p.distCss))
     .pipe(reload({stream: true}));
 });
